0609d6b95f6cd8cf9c00474b6ed7e55f6ddf0618
Yaroslav-Nester Pitsur <ypitsur@foundationmedicine.com>
2022-08-01 00:49:45 +0300

    add log file

--- test/data/types.ts
+++ test/data/types.ts
@@ -1,8 +1,20 @@
 export interface stepOptions {
-    createStep?: boolean;
-    takeScreenshot?: boolean;
-    takeViewPortScreenshot?: boolean;
-    customDescription?: string;
-    customExpectation?: string;
-    customActual?: string;
+  createStep?: boolean;
+  takeScreenshot?: boolean;
+  takeViewPortScreenshot?: boolean;
+  customDescription?: string;
+  customExpectation?: string;
+  customActual?: string;
+}
+
+export interface LogFile {
+  steps: {
+    stepNumber: number;
+    description: string;
+    expected: string;
+    actual: string;
+    status: string;
+    screenshot?: string;
+    attachment?: JSON;
+  }[];
 }
--- test/helpers/logger.ts
+++ test/helpers/logger.ts
@@ -1,16 +1,35 @@
-import { stepOptions } from '../data/types';
+import { stepOptions, LogFile } from '../data/types';
 import allure from '@wdio/allure-reporter';
+import { writeFile } from 'fs';
 
 export default class Logger {
   constructor() {}
-  static stepNumber = 0
+  private static stepNumber = 0;
+  private static logFile: LogFile = { steps: [] };
+  private static screenshot: string;
+  private static screenshotName = `./logs/log${Math.floor(new Date().getTime() + Math.random() * 1000)}.json`;
+
+  private static async handleScreenshot() {
+    const data = await browser.saveScreenshot(`./screenshots/step_${Logger.stepNumber}_test_name.png`);
+    Logger.screenshot = Buffer.from(data).toString('base64');
+    return data;
+  }
+
+  private static createLogFile() {
+    writeFile(
+      Logger.screenshotName,
+      JSON.stringify(Logger.logFile),
+      (error) => console.error(error),
+    );
+  }
+
   static async step(
     options: stepOptions = { createStep: true, takeScreenshot: false },
     description: string,
     expected: string,
     actual: string,
     assertion: Function,
-    ): Promise<void> {
+  ): Promise<void> {
     if (options.createStep || options.takeScreenshot) {
       try {
         Logger.stepNumber++;
@@ -22,34 +41,43 @@ export default class Logger {
           allure.addStep(
             allureTestTitle,
             {
-              content: await browser.saveScreenshot(
-                `./screenshots/step_${Logger.stepNumber}_test_name.png`,
-              ),
+              content: await Logger.handleScreenshot(),
               type: 'image/png',
               name: `Step ${Logger.stepNumber} screenshot`,
             },
             'passed',
           );
+          Logger.logFile.steps.push({
+            stepNumber: Logger.stepNumber,
+            description,
+            expected,
+            actual,
+            status: 'passed',
+            screenshot: Logger.screenshot,
+          });
         } else {
           allure.addStep(allureTestTitle);
+          Logger.logFile.steps.push({
+            stepNumber: Logger.stepNumber,
+            description,
+            expected,
+            actual,
+            status: 'passed',
+          });
         }
         console.log(`STEP ${Logger.stepNumber} PASSED. Actual result:\n\n${actual}\n`);
       } catch (error) {
-        await browser.saveScreenshot(
-          `./screenshots/step_${Logger.stepNumber}_failed.png`,
-        ),
-          console.error(
-            `STEP ${Logger.stepNumber} FAILED. Actual result:\n\n${actual}\n`,
-          );
+        await Logger.handleScreenshot(),
+          console.error(`STEP ${Logger.stepNumber} FAILED. Actual result:\n\n${error}\n`);
         throw error;
+      } finally {
+        Logger.createLogFile();
       }
     } else {
       try {
         await assertion();
       } catch (error) {
-        await browser.saveScreenshot(
-          `./screenshots/step_${Logger.stepNumber}_failed.png`,
-        );
+        await Logger.handleScreenshot();
         throw error;
       }
     }
